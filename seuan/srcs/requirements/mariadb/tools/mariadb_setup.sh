503697f6dd88619941e2991fe66439440f943f3d
aseungbo <seungbo1112@gmail.com>
2022-10-31 18:15:32 +0900

    fix: volume type

--- seuan/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ seuan/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -3,7 +3,7 @@
 service mysql start;
 
 cat /var/lib/mysql/.setup 2> /dev/null
-
+if [ $? -ne 0 ]; then
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
@@ -12,7 +12,7 @@ mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWO
 
 mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
 mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
-
 touch /var/lib/mysql/.setup
+fi
 
-exec mysqld --console
+exec mysqld
